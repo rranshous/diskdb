15ddd1f35584ba2e5b9aba48817210e72714f218
Robby Ranshous <rranshous@altalang.com>
2010-12-22 09:59:06 -0500

    removed print

--- Blip.py
+++ Blip.py
@@ -20,7 +20,7 @@ class SimpleBlip(object):
 
     @require_attribute('key')
     def flush(self):
-        print 'flushing'
+        #print 'flushing'
         # write our value out
         self.handler.flush(self)
         return True
