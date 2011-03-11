afd21b4d6eaa031fb34c2757b491443b525db378
Robby Ranshous <rranshous@gmail.com>
2011-03-11 17:37:50 -0500

    fixes for client, defaults to simpleblip

--- client.py
+++ client.py
@@ -3,8 +3,14 @@
 # simple client sitting in front of the Blip obj
 # for simple k/v setting / getting
 
+from Blip import SimpleBlip # default blip type
+
 class Client(object):
-    def __init__(self,storage_root):
+    def __init__(self,storage_root,blip_cls=None):
+        # they can provide their own blip class, else simple
+        self.Blip = blip_cls or SimpleBlip
+
+        # where is all this data going
         self.storage_root = storage_root
 
     def get(self,k):
